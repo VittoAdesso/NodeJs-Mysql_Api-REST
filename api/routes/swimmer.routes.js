64ce7101931123deff922a8ac07a207d213a0e61
Vittorio Adesso <adessovittorio8@gmail.com>
2022-04-13 13:58:57 +0200

    added query to create a new swimmer

--- api/routes/swimmer.routes.js
+++ api/routes/swimmer.routes.js
@@ -20,9 +20,9 @@ router.route("/swimmer").get((req, res, next) => {
 
 //GET SINGLE swimmer BY ID with params !!!  // 
 
-    router.route("/swimmer/:id").get((req, res, next) => {
-        const userId = req.params.id
-        const sqlQuery = `SELECT * FROM swimmers WHERE id=${userId}`
+router.route("/swimmer/:id").get((req, res, next) => {
+    const userId = req.params.id
+    const sqlQuery = `SELECT * FROM swimmers WHERE id=${userId}`
         connectDb.query(sqlQuery, (error, response) => {
             if (error) {
                 return next(error)
@@ -31,7 +31,7 @@ router.route("/swimmer").get((req, res, next) => {
                 res.status(200).json(response)
             }
         })
-    })
+})
 
     //GET SINGLE swimmer BY ID -- mannually  // 
 // router.route("/swimmer/1").get((req, res, next) => {
@@ -47,9 +47,25 @@ router.route("/swimmer").get((req, res, next) => {
 //                 res.status(200).json(response)
 //             }
 //         })
-//     })
-    
-/////////////////////////////////////////////////////////////////////////
+//})
+
+router.post("/swimmer/create", (req, res, next) => {
+    const inputData = {
+        swimmerId: req.body.swimmerId,
+        name: req.body.name,
+        sex: req.body.sex, 
+        createdAt: req.body.createdAt, 
+        updatedAt: req.body.updatedAt
+    }
+    connectDb.query("INSERT INTO swimmers SET ?", [inputData], (error, result) => {
+        if (error) {
+            res.status(500).send({msg: "We can't register"})
+            return next(error)
+        } else {
+            res.status(200).send({msg: "Registry is complete"})
+        }
+    })
+})
 
 // i have to export, because i need to use it in other places of the project //
 module.exports = router; 
