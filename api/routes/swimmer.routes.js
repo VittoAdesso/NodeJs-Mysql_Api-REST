64e4791a7156f4cfe75bc70285992b5a4e0cac7d
Vittorio Adesso <adessovittorio8@gmail.com>
2022-04-13 13:34:10 +0200

    added query to get swimmer by id

--- api/routes/swimmer.routes.js
+++ api/routes/swimmer.routes.js
@@ -18,6 +18,38 @@ router.route("/swimmer").get((req, res, next) => {
     })
 })
 
+//GET SINGLE swimmer BY ID with params !!!  // 
 
-  // i have to export, because i need to use it in other places of the project //
+    router.route("/swimmer/:id").get((req, res, next) => {
+        const userId = req.params.id
+        const sqlQuery = `SELECT * FROM swimmers WHERE id=${userId}`
+        connectDb.query(sqlQuery, (error, response) => {
+            if (error) {
+                return next(error)
+            }
+            else {
+                res.status(200).json(response)
+            }
+        })
+    })
+
+    //GET SINGLE swimmer BY ID -- mannually  // 
+// router.route("/swimmer/1").get((req, res, next) => {
+//     const swimmerId = 'vitAd'; 
+//     // all the querys i have to almacenar into const
+//     const sqlQuery = 'SELECT * FROM swimmers WHERE swimmerId = ?'
+//         //REMEMBER TO CALL THE CONNECTION
+//         connectDb.query(sqlQuery, [swimmerId], (err, response) => {
+//             if (err) {
+//                 return next(err)
+//             }
+//             else {
+//                 res.status(200).json(response)
+//             }
+//         })
+//     })
+    
+/////////////////////////////////////////////////////////////////////////
+
+// i have to export, because i need to use it in other places of the project //
 module.exports = router; 
