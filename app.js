9b1cf2fab2b3f9438e5a126d80010ab16f4fee88
Vittorio Adesso <adessovittorio8@gmail.com>
2022-04-16 00:01:39 +0200

    added headers

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const swimmer = require("./api/routes/swimmer.routes");
 const competition = require("./api/routes/competition.routes");
 const registration = require("./api/routes/registration.routes");
 
+// define headers to response (res) in every petition express going to create cabecere with configuration what we determinate
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
